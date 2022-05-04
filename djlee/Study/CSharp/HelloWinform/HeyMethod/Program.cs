8f703353f776d41168c523a2cbf0fdcbc0408bff
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-05-04 12:43:48 +0900

    깊은복사 얕은복사 찍먹

--- djlee/Study/CSharp/HelloWinform/HeyMethod/Program.cs
+++ djlee/Study/CSharp/HelloWinform/HeyMethod/Program.cs
@@ -49,6 +49,11 @@ namespace HeyMethod
         }
 
         //특정한 메시지를 지정한 만큼 출력하고 싶을 때
+        /// <summary>
+        /// 매개변수 2개 반환 값 없으나 상황에 따라서 종료시킴
+        /// </summary>
+        /// <param name="message">출력할 메세지</param>
+        /// <param name="count">메세지 출력 횟수</param>
         static void printMethod(string message, int count)
         {
             if(count<=0)
@@ -63,33 +68,113 @@ namespace HeyMethod
 
         static void Main(string[] args)
         {
+            if(false)
+            {
+                /// 주석도 이런 거 있긴 있다
+                /// 엔터치면 막 생기죠 ㅋㅋㅋ
+                /// 메소드 부가 설명 이걸로 많이 합니다 ㅎㅎㅎ
+                /// 
 
-            //new Program().ex(); //객체화
+                #region Main에서의 메소드 호출 연습
 
-            Console.WriteLine("숫자 입력해봐라");
-            int a = int.Parse(Console.ReadLine());
-            Console.WriteLine("숫자 또 입력해라");
-            int b = int.Parse(Console.ReadLine());
-            if (a < 0)
-                a *= -1;
-            if (b < 0)
-                b *= -1;
-            Console.WriteLine(a+b);
-            Console.WriteLine(AbsAdd(a,b)); //호출
 
-            string result = getPw(); //getPw가 호출되는 순간 나는 문자열을 입력해야 함
-            Console.WriteLine(result);
-            Console.WriteLine(getPw()); //getPw가 끝나야 그 결과를 출력할 수 있다.
 
-            int abs = AbsAdd(-100, 50);
-            Console.WriteLine("100+50의 절댓값 합은 " + abs  );
-            Console.WriteLine("100+50의 절댓값 합은 " + AbsAdd(-50,-100)  );
+                //new Program().ex(); //객체화
 
-            //기타등등 잡다부리한 것들은 내가 몰라도 됨
-            introduceMySelf();
+                Console.WriteLine("숫자 입력해봐라");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("숫자 또 입력해라");
+                int b = int.Parse(Console.ReadLine());
+                if (a < 0)
+                    a *= -1;
+                if (b < 0)
+                    b *= -1;
+                Console.WriteLine(a + b);
+                Console.WriteLine(AbsAdd(a, b)); //호출
 
-            printMethod("이동준잘생겼다", 3);
-            printMethod("이동준못생겼다", -1); //예외상황
+                string result = getPw(); //getPw가 호출되는 순간 나는 문자열을 입력해야 함
+                Console.WriteLine(result);
+                Console.WriteLine(getPw()); //getPw가 끝나야 그 결과를 출력할 수 있다.
+
+                int abs = AbsAdd(-100, 50);
+                Console.WriteLine("100+50의 절댓값 합은 " + abs);
+                Console.WriteLine("100+50의 절댓값 합은 " + AbsAdd(-50, -100));
+
+                //기타등등 잡다부리한 것들은 내가 몰라도 됨
+                introduceMySelf();
+
+                printMethod("이동준잘생겼다", 3);
+                printMethod("이동준못생겼다", -1); //예외상황
+                #endregion
+
+            }
+
+
+            if(false)
+            {
+            #region 클래스,인스턴스메소드및클래스메소드
+
+            
+            Student s = new Student();
+            s.setInfo("이동준", 34, "09");
+            Student s2 = new Student() { Age=20, Hakbeon="22", Name="정병주"};
+            Student s3 = new Student();
+            s3.Name = "문찬호";
+            s3.Age = 34;
+            s3.Hakbeon = "08";
+
+            s.introduce(); //인스턴스 메소드 호출
+            s2.introduce();
+            s3.introduce();
+
+            Student.Study(); //클래스 메소드 호출
+            //s.Study();
+            //s2.Study();
+            //s3.Study();
+
+            Student s4 = new Student("김영환", 30, "17");//학생을 선언하자마자 정보를 아는 경우
+            Student s5 = new Student();//지금은 모르고 나중에 알 수 있는 경우
+            s5.setInfo("한준범", 25, "21");
+
+            Student.Study(10);
+            #endregion
+
+            }
+
+            //class랑 struct 차이
+            //깊은 복사 얕은 복사
+
+            //깊은 복사 : 객체끼리 복사할 때 값까지 다 복사되는 것
+            //얕은 복사 : 주소만 복사됨. 그래서 복사 후 복사본의 값을 바꿀 경우엔 반드시 깊은 복사
+            //써야 한다. 그래야 원본이 영향을 받지 않는다.
+
+            //결론
+            //class는 얕은 복사를 쓰고, 깊은 복사하려면 별도의 조치 혹은 수동으로 하나하나 다 복사
+            //struct는 int나 char처럼 쉽게 복사가 됨
+
+            int aa = 100;
+            int bb = aa;
+            bb++; //bb의 값이 증가한다고 해서 aa의 값이 바뀌진 않음. 
+            Console.WriteLine("aa="+aa);
+            Console.WriteLine("bb="+bb);
+
+            Student ss1 = new Student("이동준2", 34, "1234");
+            Student ss2 = ss1; //얕은 복사
+            ss2.Age = 20;
+            ss2.Hakbeon = "22";
+
+            Console.WriteLine(ss1.Name+ss1.Age+ss1.Hakbeon); //얕은 복사의 문제점!!!
+            Console.WriteLine(ss2.Name+ss2.Age+ss2.Hakbeon);
+
+            Car c1 = new Car();
+            c1.Price = 500;
+            c1.Name = "5시리즈(k5)";
+
+            Car c2 = c1; //깊은 복사가 됨. 구조체니까...
+            c2.Name = "bmw5";
+            Console.WriteLine(c1.Name+c1.Price);
+            Console.WriteLine(c2.Name+c2.Price);
+
         }
     }
 }
--- djlee/Study/CSharp/HelloWinform/HeyMethod/Student.cs
+++ djlee/Study/CSharp/HelloWinform/HeyMethod/Student.cs
@@ -16,6 +16,28 @@ namespace HeyMethod
         //public int Grade { get { return grade; } set { grade = value; } } 
         //public int Grade { get =>  grade;  set => grade = value;  } 
 
+        //생성자
+        //인스턴스를 만듦과 동시에 바로 멤버변수들의 값들 추가해줄 수 있다.
+        public Student(string Name, int Age, string Hakbeon)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            this.Hakbeon = Hakbeon;
+        }
+        //생성자는 이름이 겹치더라도 매개변수의 개수나 타입이 다르면 얼마든지 여러 개 쓸 수 있다.
+        //근데 이 법칙은 메소드(인스턴스메소드든 클래스메소드든 상관없이) 에서도 적용이 됩니다.
+        //이렇게 이름만 겹치고 매개변수가 다른 걸 "오버로딩"이라고 한다!!!
+        
+
+        //class 만들 때 생성자를 안 만들면... 아래와 같이 비어있는 생성자가 
+        //자동으로 하나 만들어짐(암묵적으로)
+        //근데 매개변수가 있는 생성자 혹은 매개변수는 없는 데 뭔가 기능을 하는 생성자를 하나
+        //만들 게 되면 기존의 비어있는 생성자는 삭제가 된다.
+        public Student()
+        {
+
+        }
+
         public void setInfo(string Name, int Age, string Hakbeon)
         {
             //this는 인스턴스 변수 즉 멤버변수를 가리킴
@@ -24,15 +46,24 @@ namespace HeyMethod
             this.Age = Age;
             this.Hakbeon = Hakbeon;
         }
+        //인스턴스 메소드
+        //학생"별로 각각 다르게" 하는 기능
         public void introduce()
         {
             Console.WriteLine("이름 : " + Name);
             Console.WriteLine("나이 : " + Age);
             Console.WriteLine("학번 : " + Hakbeon);
         }
-        public void Stduy()
+        //클래스 메소드
+        //공통 기능
+        //Student 클래스 자체의 기능
+        public static void Study() //학생들이 전부 다 "공통적으로" 하는 기능
         {
             Console.WriteLine("열심히 공부를 합니다. 아뵤~");
+        }
+        public static void Study(int hour)
+        {
+            Console.WriteLine(hour+"시간 공부를 합니다.");
         }
     }
 }
