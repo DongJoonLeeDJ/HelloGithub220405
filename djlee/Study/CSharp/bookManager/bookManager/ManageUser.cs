7a6f6d275ccdbd8239442f53adbf87bcc09c3132
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-05-10 16:03:21 +0900

    수정까지 완료

--- djlee/Study/CSharp/bookManager/bookManager/ManageUser.cs
+++ djlee/Study/CSharp/bookManager/bookManager/ManageUser.cs
@@ -31,6 +31,14 @@ namespace bookManager
 
         private void button_add_Click(object sender, EventArgs e)
         {
+            //숫자 0은 임의의 값이고, Books에서 userId가 없을 때 나타내는 임의의 값이므로
+            //0은 Id로 쓰지 않는다.
+            if(int.Parse(textBox_id.Text) ==0)
+            {
+                MessageBox.Show("잘못된 ID입니다.");
+                return;
+            }
+
             //해당 id 존재하는지 여부 체크 Exists
             if(DataManager.Users.Exists(x=>x.Id == int.Parse(textBox_id.Text)))
             {
@@ -49,6 +57,32 @@ namespace bookManager
         private void button_modify_Click(object sender, EventArgs e)
         {
             //Single사용
+            //Single : List에서 조건을 만족하는 인스턴스 하나를 가리키는 것
+            //조건을 만족하는 것이 List안에 없으면 catch부분으로 빠짐(에러를 throw)
+            try
+            {
+                User user = DataManager.Users.Single(x => x.Id == int.Parse(textBox_id.Text));
+                user.Name = textBox_name.Text;
+
+                //만약에 해당 유저가 책을 빌린 유저였다면, Books의 userName정보도 같이 바꿔야 됨
+                try
+                {
+                    Book book = DataManager.Books.Single(x => x.UserId == int.Parse(textBox_id.Text));
+                    book.UserName = textBox_name.Text;
+                }
+                catch (Exception) //해당 유저가 책을 안 빌렸을 수도 있으니까 없으면 냅둔다.
+                {
+                }
+
+                dataGridView_Users.DataSource = null;
+                dataGridView_Users.DataSource = DataManager.Users;
+                DataManager.Save();
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("이 아이디는 없는 유저입니다.");
+            }
         }
 
         private void button_delete_Click(object sender, EventArgs e)
