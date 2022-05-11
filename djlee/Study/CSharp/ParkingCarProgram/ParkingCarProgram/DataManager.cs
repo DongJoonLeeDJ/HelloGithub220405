52ec16a2b4a362df96de885d22a8cf10ec239250
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-05-11 12:42:28 +0900

    하는중

--- djlee/Study/CSharp/ParkingCarProgram/ParkingCarProgram/DataManager.cs
+++ djlee/Study/CSharp/ParkingCarProgram/ParkingCarProgram/DataManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,29 @@ namespace ParkingCarProgram
 {
     public class DataManager
     {
+        public static List<ParkingCar> Cars = new List<ParkingCar>();
+        static DataManager()
+        {
+            Load();
+        }
+        public static void Load()
+        {
+            try
+            {
+                DBHelper.selectQuery();
+                Cars.Clear(); //기존에 내용 지움
+                foreach (DataRow item in DBHelper.dt.Rows)
+                {
+                    ParkingCar temp = new ParkingCar();
+                    temp.ParkingSpot = int.Parse(item["ParkingSpot"].ToString());
+                    temp.CarNumber = item["CarNumber"].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
     }
 }
